51ce918e3d61f06ceed801e378fec4ad30b9c554
andresr31 <carlos.andres.1031@gmail.com>
2020-05-25 02:27:55 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
+import { WelcomeComponent } from './Home/welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: WelcomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
